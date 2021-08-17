f764f7e0fa687247e936ba6069dde5f03bec5361
kat-dev-coder <72075396+kat-dev-coder@users.noreply.github.com>
2021-08-17 14:58:40 +0200

    game working

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let deckID
+let cardPosition = 0;
 
 
 
@@ -8,31 +9,34 @@ const newTryBtn = document.getElementById('newTry')
 const cardsLeft = document.getElementById('cardsLeft')
 let cardsRemaining
 const newCardBtn = document.getElementById('newCard')
-let card = document.getElementById('card')
-const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"
-
-]
-let cardsArray = []
 const checkBtn = document.getElementById('check')
+let card = document.getElementById('card')
+const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
+let cardsArray = []
+
 
 newTryBtn.addEventListener('click', getNewDeck)
 function getNewDeck() {
-    console.log('button works')
+    card.innerHTML = `
+    <img src="img/playing-card-568200_640.jpg" alt="">
+    `
+    cardsArray = []
+    cardsLeft.textContent = cardsArray.length
+    cardPosition = 0;
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
-    .then(res => res.json())
-    .then(data => {
-        setButton()
-        // cardsRemaining = data.remaining
-        // cardsLeft.innerHTML = `Noch ${cardsRemaining} Karten übrig`
-        // console.log(cardsRemaining)
-        
-        deckId = data.deck_id
-        console.log(deckId)
-    
-    
-    })
+        .then(res => res.json())
+        .then(data => {
+            setButton()
+            // cardsRemaining = data.remaining
+            // cardsLeft.innerHTML = `Noch ${cardsRemaining} Karten übrig`
+            // console.log(cardsRemaining)
 
-    
+            deckId = data.deck_id
+
+
+        })
+
+
 
 }
 newCardBtn.addEventListener('click', newCard)
@@ -40,54 +44,51 @@ newCardBtn.addEventListener('click', newCard)
 function newCard() {
     console.log(deckId)
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-    .then(res => res.json())
-    .then(data => {
-        cardsArray.push(data.cards[0].value)
-        card.innerHTML = `
+        .then(res => res.json())
+        .then(data => {
+            cardsArray.push(data.cards[0].image)
+            console.log(cardsArray)
+            card.innerHTML = `
         <img src=${data.cards[0].image}>
         `
-        cardsLeft.innerHTML = `
-        <h3>Anzahl der Karten: <br>${cardsArray.length}</h3>
-    `
-       
-        if(cardsArray.length === 10) {
-            newCardBtn.disabled = true;
-            checkBtn.disabled = false;
-            cardsLeft.innerHTML += `
-            <h3>Ende des Durchgangs</h3>
+            cardsLeft.textContent = cardsArray.length
+
+            if (cardsArray.length === 10) {
+                newCardBtn.disabled = true;
+                checkBtn.disabled = false;
+                document.getElementById('span-cards-left').classList.add('display-none')
+                cardsLeft.innerHTML = `
+            <span>Ende des Durchgangs</span>
         `
-        } 
-       
+            }
 
 
-    })
+
+        })
 }
 function saveArray() {
-    if(cardsRemaining === 42) {
+    if (cardsRemaining === 42) {
         cardsArray
     }
 }
 
 function setButton() {
     newCardBtn.disabled = false;
-    if(cardsRemaining === 0) {
-        newCardBtn.disabled = true; 
+    if (cardsRemaining === 0) {
+        newCardBtn.disabled = true;
     }
 }
+checkBtn.addEventListener('click', checkCards)
 
-// function testAPI() {
-//     fetch('https://api.unsplash.com/photos/random')
-// }
-// function getDate() {
-// const date = new Date()
-// let timeGermany = date.toLocaleTimeString('de-DE', {timeStyle: "short"})
-// let timeEngland = date.toLocaleTimeString('en-EN', {timeStyle: "short"})
-// document.getElementById('time-germany').innerHTML = `
-//     <p>Uhrzeit in Deutschland: ${timeGermany}</p>
-//     <p>Uhrzeit in England: ${timeEngland}
-// `
- 
-// }
+function checkCards() {
 
-
-    
+    card.innerHTML = `
+    <img src="${cardsArray[cardPosition]}" alt="">
+    `
+    cardPosition++
+    document.getElementById('span-cards-left').classList.remove('display-none')
+    cardsLeft.textContent = cardPosition
+    if(cardPosition === 10) {
+        checkBtn.disabled = true;
+    }
+}
